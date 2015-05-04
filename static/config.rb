48ee07292f2e6f9c5df7a82588be40dcf5cb214c
= <=>
2015-05-04 14:34:40 +0000

    se agregan cambios de server

---
+++ static/config.rb
@@ -0,0 +1,17 @@
+# General Frontend settings
+# 30 / 03 / 2015
+
+http_images_path = "/static/imgs"
+http_fonts_path = "fonts"
+http_path = "/static"
+css_dir = "build/css"
+sass_dir = "styles"
+images_dir = "imgs"
+javascripts_dir = "js"
+project_path = "content"
+preferred_syntax = :sass
+relative_assets = true
+output_style = :compressed
+line_comments = false
+
+# You can select your preferred output style here (can be overridden via the command line):
