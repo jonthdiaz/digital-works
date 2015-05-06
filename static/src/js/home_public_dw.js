181c7db071ef02c6414b1cf3bf018ac623ffb49a
jonthdiaz <jonthdiaz@gmail.com>
2015-05-05 23:34:33 -0500

    static

---
+++ static/src/js/home_public_dw.js
@@ -0,0 +1,26 @@
+require.config({
+    paths:{
+        'jquery':'../../components/jquery/dist/jquery',
+        'angular':'../../components/angular/angular',
+        'angularAMD':'../../components/angularAMD/angularAMD',
+        'bootstrap': '../../components/bootstrap-sass-official/assets/javascripts/bootstrap'
+    },
+    shim:{
+        'jquery':{
+            exports : 'jQuery'
+        },
+        'angular': {
+            exports: 'angular',
+        },
+        'angularAMD':['angular'],
+        'bootstrap':{
+            deps: ['jquery']
+        }
+    },
+});
+
+require(['jquery', 'bootstrap'],function  ($, bootstrap) {
+   $(document).ready(function  () {
+       
+   });
+});
