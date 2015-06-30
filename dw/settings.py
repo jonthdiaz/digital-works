a8bfe13e1ba027a0ae896b6b9a5d25fb25d2e9eb
jonthdiaz <jonthdiaz@gmail.com>
2015-06-30 17:16:40 -0500

    se agrega

--- dw/settings.py
+++ dw/settings.py
@@ -110,7 +110,8 @@ MIDDLEWARE_CLASSES = (
     'cms.middleware.user.CurrentUserMiddleware',
     'cms.middleware.page.CurrentPageMiddleware',
     'cms.middleware.toolbar.ToolbarMiddleware',
-    'cms.middleware.language.LanguageCookieMiddleware'
+    'cms.middleware.language.LanguageCookieMiddleware',
+    'django.middleware.gzip.GZipMiddleware',
 
 )
 
