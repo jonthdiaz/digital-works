e655eff71b0c1b2e3e73d5637649df1123b3a1aa
jonthdiaz <jonthdiaz@gmail.com>
2015-07-11 11:16:41 -0500

    bugs de qa

--- content/angular/home/init_dom.js
+++ content/angular/home/init_dom.js
@@ -2,6 +2,5 @@ define(function  (require) {
    var $ =  require("jquery");
    var bootstrap =  require("bootstrap");
    $(document).ready(function  () {
-    console.log("esto es una prueba para el dom");
    });
 });
